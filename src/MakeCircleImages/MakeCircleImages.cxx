ff9242a307f4788168cf40b2c99c6f8ccd7ad190
nkrchak <noah-krchak@uiowa.edu>
2026-04-13 13:08:30 -0500

    ENH: Added necessary headers for MakeCircleImages algorithm and created the main function

--- src/MakeCircleImages/MakeCircleImages.cxx
+++ src/MakeCircleImages/MakeCircleImages.cxx
@@ -1,3 +1,15 @@
 //
 // Created by nkrchak on 4/13/26.
 //
+
+
+
+#include <itkEllipseSpatialObject.h>
+#include "itkImageFileWriter.h"
+
+int main(int argc, char *argv[])
+{
+
+
+    return EXIT_SUCCESS;
+}
