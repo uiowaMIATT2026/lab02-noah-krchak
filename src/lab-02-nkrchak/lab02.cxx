91c1b1e309464088fbdafb7f39ad004293fac5fe
nkrchak <noah-krchak@uiowa.edu>
2026-04-13 15:04:37 -0500

    ENH: Created the base template for lab02.cxx

--- src/lab-02-nkrchak/lab02.cxx
+++ src/lab-02-nkrchak/lab02.cxx
@@ -1,3 +1,19 @@
 //
 // Created by nkrchak on 4/13/26.
 //
+#include "lab02CLP.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImageRegistrationMethodv4.h"
+#include "itkAffineTransform.h"
+#include "itkMeanSquaresImageToImageMetricv4.h"
+#include "itkRegularStepGradientDescentOptimizerv4.h"
+#include "itkCompositeTransform.h"
+#include "itkResampleImageFilter.h"
+
+
+int main( int argc, char *argv[] )
+{
+    PARSE_ARGS;
+    return EXIT_SUCCESS;
+}
