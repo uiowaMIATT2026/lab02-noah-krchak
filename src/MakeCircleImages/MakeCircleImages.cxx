0ce94fc66085ebd220ecfe2923ddd45f4f1957c4
nkrchak <noah-krchak@uiowa.edu>
2026-04-13 13:20:55 -0500

    ENH: Set the origin and spacing for img1 in MakeCircleImage.cxx

--- src/MakeCircleImages/MakeCircleImages.cxx
+++ src/MakeCircleImages/MakeCircleImages.cxx
@@ -14,6 +14,21 @@ using ImageType = itk::Image<PixelType, numDim>;
 
 int main(int argc, char *argv[])
 {
+    ImageType::Pointer img1 = ImageType::New();
+
+    //Set spacing for img1
+    ImageType::SpacingType img1Spacing;
+
+    img1Spacing[0] = 1.0; // Spacing along x
+    img1Spacing[1] = 1.0; // Spacing along y
+
+    img1->SetSpacing(img1Spacing);
+
+    //Set origin for img1
+    const ImageType::PointType img1Origin{{0.0, 0.0}}; // x: 0.0 mm, y: 0.0 mm
+
+    img1->SetOrigin(img1Origin);
+
 
 
     return EXIT_SUCCESS;
