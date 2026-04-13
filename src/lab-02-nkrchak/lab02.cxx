4037afb7bf36a8d7339c4e8abfebb949e1102a07
nkrchak <noah-krchak@uiowa.edu>
2026-04-13 15:14:16 -0500

    ENH: Added all the types required for registration

--- src/lab-02-nkrchak/lab02.cxx
+++ src/lab-02-nkrchak/lab02.cxx
@@ -11,6 +11,29 @@
 #include "itkCompositeTransform.h"
 #include "itkResampleImageFilter.h"
 
+const int numDim = 2;
+using InputPixelType = float;
+using AffineTransformPixelType = double;
+using OptimizerValueType = double;
+using InterpolatorCoordinateValueType = double;
+using OutputPixelType = unsigned char;
+
+using FixedImageType = itk::Image<InputPixelType, numDim>;
+using MovingImageType = itk::Image<InputPixelType, numDim>;
+
+using OutputImageType = itk::Image<OutputPixelType, numDim>;
+
+using FixedImageReaderType = itk::ImageFileReader<FixedImageType>;
+using MovingImageReaderType = itk::ImageFileReader<MovingImageType>;
+using ImageWriterType = itk::ImageFileWriter<OutputImageType>;
+
+using AffineTransformType = itk::AffineTransform<AffineTransformPixelType, numDim>;
+using OptimizerType = itk::RegularStepGradientDescentOptimizerv4<OptimizerValueType>;
+using MetricType = itk::MeanSquaresImageToImageMetricv4<FixedImageType, MovingImageType>;
+using RegistrationType = itk::ImageRegistrationMethodv4<FixedImageType, MovingImageType, AffineTransformType>;
+
+using FixedInterpolatorType = itk::LinearInterpolateImageFunction<FixedImageType, InterpolatorCoordinateValueType>;
+using MovingInterpolatorType = itk::LinearInterpolateImageFunction<MovingImageType, InterpolatorCoordinateValueType>;
 
 int main( int argc, char *argv[] )
 {
